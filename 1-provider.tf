b4b849c5fbb4e42ffd7d14fbae1be6883ea223d1
Anudeep Nalla <anudeep.nalla@nirmata.com>
2023-06-07 15:52:58 +0530

    gke tf code for autopilot private cluster

---
+++ 1-provider.tf
@@ -0,0 +1,6 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  #project = "optical-figure-385509"
+  project = "zee-ott-private"
+  region  = "asia-south1"
+}
---
+++ 6-firewalls.tf
@@ -0,0 +1,24 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow-kyverno" {
+  name    = "allow-kyverno"
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9443"]
+  }
+
+  source_ranges = ["10.0.0.0/18", "10.48.0.0/14", "10.52.0.0/20", "172.16.0.0/28"]
+}
